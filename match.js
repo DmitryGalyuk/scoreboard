44c458b52d7a7a7b50026840619da4b6af2917fb
Dmitry Galyuk <dmitry@galyuk.com>
2023-09-25 11:07:30 +0400

    match constructor

--- match.js
+++ match.js
@@ -1,3 +1,8 @@
 export default class Match {
-    
+    constructor(homeTeam, awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
+    }
 }
