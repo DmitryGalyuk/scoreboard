78d913dbce6a531a5fde1fdf44cb936f5377f718
Dmitry Galyuk <dmitry@galyuk.com>
2023-09-25 13:00:08 +0400

    match start

--- scoreboard.js
+++ scoreboard.js
@@ -1,3 +1,18 @@
+import Match from "./match";
+
 export default class Scoreboard {
-    
+    constructor() {
+        this.matches = [];
+    }
+
+    getMatch(homeTeam, awayTeam) {
+        const result = this.matches.find((m)=>{return m.homeTeam===homeTeam && m.awayTeam===awayTeam});
+        if (result)
+            return result;
+        throw new Error("Match not found");
+    }
+
+    startMatch(homeTeam, awayTeam) {
+        this.matches.push(new Match(homeTeam, awayTeam));
+    }
 }
