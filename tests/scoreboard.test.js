7cc194b65e1e3c67f48283506191a2c6060307fb
Dmitry Galyuk <dmitry@galyuk.com>
2023-09-25 12:44:55 +0400

    match start test

--- tests/scoreboard.test.js
+++ tests/scoreboard.test.js
@@ -3,9 +3,15 @@ import Scoreboard from "../scoreboard"
 
 test("Scoreboard exists", ()=> {
     expect(new Scoreboard()).toBeDefined();
-})
+});
 
-test("Match can be added");
+test("Match can be started, score is 0-0", ()=>{
+    const board = new Scoreboard();
+    board.startMatch("home", "away");
+    expect(board.getMatch("home", "away").homeScore).toEqual(0);
+    expect(board.getMatch("home", "away").awayScore).toEqual(0);
+});
+
 test("Match score can be updated");
 test("Match can be finished");
 test("Summary is ordered based on the total score");
