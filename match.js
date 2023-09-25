6d3cea314078c2380edf368b57a9937773aa13e5
Dmitry Galyuk <dmitry@galyuk.com>
2023-09-25 11:57:14 +0400

    match creation, score update

--- match.js
+++ match.js
@@ -1,8 +1,25 @@
 export default class Match {
     constructor(homeTeam, awayTeam) {
+        if (
+            typeof homeTeam !== "string" || typeof awayTeam !== "string"
+            || homeTeam.length === 0 || awayTeam.length === 0
+        ) {
+            throw new Error("Team name is mandatory and should be string")
+        }
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
         this.homeScore = 0;
         this.awayScore = 0;
+    }
+
+    updateScore(homeScore, awayScore) {
+        if (typeof homeScore !== "number" || typeof awayScore !== "number"
+            || !Number.isInteger(homeScore) || !Number.isInteger(awayScore)
+            || homeScore < 0 || awayScore < 0
+        ) {
+            throw new Error("Scores must be non-negative integers, both values are mandatory mandatory");
+        }
+        this.homeScore = homeScore;
+        this.awayScore = awayScore
     }
 }
--- tests/match.test.js
+++ tests/match.test.js
@@ -1,12 +1,43 @@
-import {test, expect}  from '@jest/globals'
+import { test, expect } from '@jest/globals'
 import Match from "../match"
 
-test("Match exists", ()=> {
-    expect(new Match()).toBeDefined();
+test("Match exists", () => {
+    expect(new Match("home", "away")).toBeDefined();
 })
 
-test("Match created with 0:0 score", ()=> {
+test("Match created with 0:0 score", () => {
     const match = new Match("home team", "away team");
     expect(match.homeScore).toEqual(0);
     expect(match.awayScore).toEqual(0);
 })
+
+test("Match score updated", () => {
+    const match = new Match("home team", "away team");
+    match.updateScore(2, 3);
+    expect(match.homeScore).toEqual(2);
+    expect(match.awayScore).toEqual(3);
+})
+
+test("Match accepts only non-negative integers as scores", () => {
+    const match = new Match("home team", "away team");
+    expect(() => { match.updateScore(-1, 3) }).toThrow();
+    expect(() => { match.updateScore(1, -3) }).toThrow();
+    expect(() => { match.updateScore("abc", 3) }).toThrow();
+    expect(() => { match.updateScore(1, "def") }).toThrow();
+    expect(() => { match.updateScore(1.3, 3) }).toThrow();
+    expect(() => { match.updateScore(1, 3.4) }).toThrow();
+})
+
+test('Match should allow only non-empty string team names', () => {
+    expect(() => {new Match('', 'away')}).toThrow();
+    expect(() => {new Match('home', '')}).toThrow();
+
+    expect(() => {new Match('home', 3)}).toThrow();
+    expect(() => {new Match(3, 'away')}).toThrow();
+    
+    expect(() => {new Match('home', {})}).toThrow();
+    expect(() => {new Match({}, 'away')}).toThrow();
+    
+    expect(() => {new Match('home', [])}).toThrow();
+    expect(() => {new Match([], 'away')}).toThrow();
+});
