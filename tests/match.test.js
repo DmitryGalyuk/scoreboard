aa2e143012f1d3bcc43c55cbc70bd6c61d609628
Dmitry Galyuk <dmitry@galyuk.com>
2023-09-25 11:05:57 +0400

    match creation test

--- tests/match.test.js
+++ tests/match.test.js
@@ -4,3 +4,9 @@ import Match from "../match"
 test("Match exists", ()=> {
     expect(new Match()).toBeDefined();
 })
+
+test("Match created with 0:0 score", ()=> {
+    const match = new Match("home team", "away team");
+    expect(match.homeScore).toEqual(0);
+    expect(match.awayScore).toEqual(0);
+})
